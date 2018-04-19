52da2669443beb8fa3f92125eb4528c870bfe118
speedy <speed.of.lightt@gmail.com>
2018-04-19 22:34:53 +0800

    Use roboto mono font

--- app/time-list/time-box.js
+++ app/time-list/time-box.js
@@ -17,7 +17,8 @@ export class TimeBox extends Tag {
         const v = this.opts.at.setZone(t.zone)
         this.str = {
             date: v.toISODate(),
-            time: v.toFormat('HH:mm:ssZZZ'),
+            time: v.toFormat('HH:mm:ss'),
+            z: v.toFormat('ZZZ'),
             zone: v.toFormat('z')
         }
     }
